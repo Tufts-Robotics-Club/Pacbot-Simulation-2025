609f0a50fc51cdca8e04faee1a9794d8497efb6d
Jordan Gasaatura <jgasaatura2006@gmail.com>
2025-11-08 13:33:16 -0500

    Maze simulator code

--- simulator/initialize.py
+++ simulator/initialize.py
@@ -1,8 +1,12 @@
 def load_maze(file_path):
     import json
-    with open(file_path, 'r') as f:
-        maze_data = json.load(f)
-    return maze_data
+    try:
+        with open(file_path, 'r') as f:
+            maze_data = json.load(f)
+        return maze_data
+    except FileNotFoundError:
+        print(f"Error: '{file_path}' not found.")
+        
 
 def initialize_simulation(maze_file):
     maze = load_maze(maze_file)
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,10 +1,17 @@
 import pygame
 import zmq
+import initialize as my_maze
+import string
+from pprint import pprint
 
 # Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+
+# initialize maze
+width, height, maze = my_maze.initialize_simulation("/Users/jordangasaatura/Desktop/Pacbot-Simulation-2025/simulator/mazes/mini.json")
+
+screen = pygame.display.set_mode((width*100, height*100))
 
 # Set up ZeroMQ server (non-blocking)
 context = zmq.Context()
@@ -37,10 +44,25 @@ while running:
     # Render
     screen.fill((0, 0, 0))
     text_surface = font.render(message_text, True, (255, 255, 255))
-    screen.blit(text_surface, (50, 50))
+
+    # zip does loop for two things at once
+    # for every row in the maze, for every row_number 0 to height
+    # then for every col in the row (row is an array),
+    for row, row_number in zip(maze, range(height)):
+        for col, col_number in zip(row, range(width)):
+            if col == 1:
+                square_color = (0, 0, 255)  # Blue
+                square_x = row_number*100
+                square_y = col_number*100
+                square_size = 100
+                #needs x and y coordinate, height and width of the square is square_size
+                square_rect = pygame.Rect(square_y, square_x, square_size, square_size)
+                pygame.draw.rect(screen, square_color, square_rect)
+
     pygame.display.flip()
     
     clock.tick(60)  # 60 FPS
+
 
 pygame.quit()
 socket.close()
